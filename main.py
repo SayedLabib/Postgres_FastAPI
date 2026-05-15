480fc3e50d3b44b8e075128d9dd49ca939e24777
SayedLabib <alfuad1007@gmail.com>
2026-05-15 23:14:32 +0600

    updated With book create and get all the books

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from fastapi import FastAPI, Depends, HTTPException
-import services, models, schemas
-
-from db import get_db, create_tables
+from services import get_book, create_book
+from schemas import BookCreate, BookBase, Book
+from db import get_db
 from sqlalchemy.orm import Session
 
 
 app = FastAPI()
 
-@app.get("/books", response_model=list[schemas.Book])
+@app.post("/books/", response_model=BookBase)
+def create_new_book(book: BookCreate, db: Session = Depends(get_db)):
+    return create_book(db, book)
 
+
+@app.get("/books/", response_model=list[Book])
 def get_all_books(db: Session = Depends(get_db)):
-    return services.get_book(db)
+    return get_book(db)
+
